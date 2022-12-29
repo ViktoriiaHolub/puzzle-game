e263bc90eeb08b1ae4636b197604524664e6debd
Victoria Holub <victoria_holub@epam.com>
2022-12-29 21:29:53 +0300

    access the camera

---
+++ script.js
@@ -0,0 +1,49 @@
+let VIDEO = null;
+let CANVAS = null;
+let CONTEXT = null;
+let SCALER = 0.8; // about how much screen space will be used bt image
+// 10% of margin will be right and left from video
+let SIZE = { x: 0, y: 0, width: 0, height: 0 };
+
+function main() {
+  CANVAS = document.getElementById("myCanvas");
+  CONTEXT = CANVAS.getContext("2d");
+
+  let promise = navigator.mediaDevices.getUserMedia({ video: true });
+  promise
+    .then(function (signal) {
+      VIDEO = document.createElement("video");
+      VIDEO.srcObject = signal;
+      VIDEO.play();
+
+      VIDEO.onloadeddata = function () {
+        handleResize();
+        // window.addEventListener("resize", handleResize); // when screen size changes
+        updateCanvas();
+      };
+    })
+    .catch(function (err) {
+      console.log("signal error", err);
+    });
+}
+
+function handleResize() {
+  // formula for location video in canvas
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight;
+  const resizer =
+    SCALER *
+    Math.min(
+      window.innerWidth / VIDEO.videoWidth,
+      window.innerHeight / VIDEO.videoHeight
+    );
+  SIZE.width = resizer * VIDEO.videoWidth;
+  SIZE.height = resizer * VIDEO.videoHeight;
+  SIZE.x = window.innerWidth / 2 - SIZE.width / 2;
+  SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
+}
+
+function updateCanvas() {
+  CONTEXT.drawImage(VIDEO, SIZE.x, SIZE.y, SIZE.width, SIZE.height);
+  window.requestAnimationFrame(updateCanvas); // for making video;
+}
